4953345379c1354b05eb784800d0be409d1984b4
MaorDora <maordora2005@gmail.com>
2025-11-14 20:34:27 +0200

    באמת שאין לי שליטה לחלוטין

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,40 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// עוד מעט ניצור את הקבצים האלה
+// import App from './App.tsx' 
+// import LoginPage from './pages/LoginPage.tsx' 
+
+// import './index.css' // אם יש לך קובץ CSS כללי, נחזיר את זה
+
+// --- הגדרת הניווט ---
+const router = createBrowserRouter([
+  {
+    path: "/",
+    // בינתיים נשים טקסט פשוט עד שניצור את העמוד
+    element: <div>טוען את אפליקציית React...</div>,
+    // TODO: כשיהיה לנו עמוד, נחליף ל:
+    // element: <App />, 
+  },
+  {
+    path: "/login",
+    // בינתיים נשים טקסט פשוט
+    element: <div>זה יהיה עמוד הלוגין</div>,
+    // TODO: כשיהיה לנו עמוד, נחליף ל:
+    // element: <LoginPage />,
+  },
+]);
+// --------------------
+
+
+// זה הקוד שמפעיל את האפליקציה
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    {/* שינוי מרכזי: 
+      במקום להציג את <App />, אנחנו מציגים את ה-RouterProvider 
+      והוא יחליט איזה עמוד להציג לפי ה-URL
+    */}
+    <RouterProvider router={router} />
+  </React.StrictMode>,
 )
